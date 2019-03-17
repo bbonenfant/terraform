36cdc18a9af4bec4be1e15a0d31ca5521fb4890f
Douglas Russo <russod37@gmail.com>
2019-03-16 20:50:33 -0400

    Completed rainflow model to work with stalgo-generated terrains.

--- terrain/tests/test_rainflow.py
+++ terrain/tests/test_rainflow.py
@@ -1,11 +1,16 @@
 import os
 import pytest
-from terrain.Object import RainFlow
+from terrain.RainFlow import RainFlow
+from terrain.Object import Object
+
 
 class TestRainFlow:
     """ Class for testing RainFlow functionality. """
     test_data_directory = os.path.join(os.path.dirname(__file__), 'test_data')
     sloped_mesh_grid_file = os.path.join(test_data_directory, 'mesh_grid_sloped.obj')
+    rotated_mesh_grid_file = os.path.join(test_data_directory, 'mesh_rotated_grid.obj')
+    terrain = Object(sloped_mesh_grid_file)
+    rotated_terrain = Object(rotated_mesh_grid_file)
 
     def test_initialize_rainflow (self):
         """ Test that a rainflow mesh can be initialized over a terrain. """
@@ -13,7 +18,7 @@ class TestRainFlow:
         expected_mesh = '1111\n1111\n1111\n1111\n'
 
         # Act
-        mesh = RainFlow(self.sloped_mesh_grid_file, 1, 1)
+        mesh = RainFlow(self.terrain, 1, 1)
 
         # Assert
         assert expected_mesh == mesh.__repr__()
@@ -21,10 +26,10 @@ class TestRainFlow:
     def test_simulate (self):
         """ Test that stepping the rainflow process on a sloped plane drains the water to the lowest cells. """
         # Arrange
-        expected_mesh = '0000\n0000\n0000\n4444\n'
+        expected_mesh = '4444\nXXXX\nXXXX\nXXXX\n'
 
         # Act
-        mesh = RainFlow(self.sloped_mesh_grid_file, 1, 1)
+        mesh = RainFlow(self.terrain, 1, 1)
         mesh.simulate(10)
 
         # Assert
